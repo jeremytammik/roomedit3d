fa6642960fe17b5ffd91e13d57ecce9bda836b7a
Jeremy Tammik <jeremy.tammik@eur.autodesk.com>
2016-05-19 12:08:32 +0200

    added config file for setting up heroku

--- config/config-view-and-data.js
+++ config/config-view-and-data.js
@@ -1,12 +1,9 @@
-/////////////////////////////////////////////////////////////////////
-// Server config file for view-and-data package
-//
-/////////////////////////////////////////////////////////////////////
+// roomedit3d/config/config-view-and-data.js
+
+// View and Data API config file
 
 module.exports = {
-
   credentials: {
-
     ConsumerKey: process.env.ROOMEDIT3D_CONSUMERKEY,
     ConsumerSecret: process.env.ROOMEDIT3D_CONSUMERSECRET
   }
