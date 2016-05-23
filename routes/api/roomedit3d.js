b39309f2a742efa14ac34822913626cb0ac71c63
Jeremy Tammik <jeremy.tammik@eur.autodesk.com>
2016-05-23 17:39:35 +0200

    cleaned up for publication

--- routes/api/roomedit3d.js
+++ routes/api/roomedit3d.js
@@ -17,14 +17,5 @@ module.exports = function(io) {
     return res.send();
   });
 
-  // implement a GET for the current transform?
-  // no, use a socket connection instead.
-  //router.get('/transform', function (req, res) {
-  //});
-
   return router;
 }
-
-// in the browser, the extension calls POST server/api/roomedit3d/transform
-// Revit add-in calls GET server/api/roomedit3d/transform
-// better: Revit add-in is notified by push via socket.io
